5278fdd1dd01217536140a4e3cee99ed6715310f
Mikhail Mandrykin <mandrykin@ispras.ru>
2020-09-09 12:26:53 +0300

    WIP: Proof_Context

--- Proof_Context.thy
+++ Proof_Context.thy
@@ -59,12 +59,106 @@ structure Basic_Util : BASIC_UTIL = Util
 open Basic_Util
 \<close>
 
-lemma prem: "A a x \<Longrightarrow> B b y \<Longrightarrow> C A B" sorry
-lemma g: "(\<And> A B x y. A a x \<Longrightarrow> B b y \<Longrightarrow> C A B) \<Longrightarrow> G" sorry
-ML \<open>@{thm prem} CCOMP @{thm g}\<close>
+ML \<open>
+structure Elim_Obtain = struct
+  fun meta_eq e = e RS @{thm atomize_eq[THEN equal_elim_rule2]}
+  val prod_conv_thms = [@{thm fst_conv}, @{thm snd_conv}] |> map meta_eq
+  fun prod_sel_tac ctxt =
+    rewrite_goal_tac ctxt prod_conv_thms THEN'
+    resolve_tac ctxt [@{thm refl}]
+  fun nth_conv ctxt n k =
+    let
+      val (xs, Ts) =
+        ("a", Name.aT) |>
+        apply2 (fn s => Name.invent Name.context s n) ||>
+        map (fn v => TFree (v, Sign.defaultS \<^theory>))
+      val ts = xs ~~ Ts |> map Free
+      val t = nth ts (k - 1)
+      val (last, m) = if n = k then (HOLogic.mk_snd, n - 2) else (HOLogic.mk_fst, k - 1)
+      val lhs = HOLogic.mk_tuple ts |> funpow m HOLogic.mk_snd |> last
+    in
+      Goal.prove ctxt xs [] (HOLogic.mk_eq (lhs, t) |> HOLogic.mk_Trueprop) (HEADGOAL (prod_sel_tac ctxt) |> K)
+    end
+  structure Rule = Proof_Data (type T = thm option val init = K NONE)
+  val True_simp = @{lemma "(True \<and> A) = A" by simp} |> meta_eq
+  fun conj_elim_tac ctxt =
+    REPEAT_ALL_NEW (eresolve_tac ctxt [@{thm conjE}]) THEN'
+    assume_tac ctxt
+  fun export ctxt vars chyps thm =
+    let
+      val phyps = map Thm.term_of chyps
+      val hyps = map HOLogic.dest_Trueprop phyps
+      val conj =
+        hyps |>
+        List.foldl (HOLogic.mk_conj o swap) \<^term>\<open>True\<close> |>
+        Raw_Simplifier.rewrite_term (Proof_Context.theory_of ctxt) [True_simp] []
+      val pconj = HOLogic.mk_Trueprop conj
+      val intros =
+        map
+          (curry Logic.mk_implies pconj #>
+           (fn g => HEADGOAL (conj_elim_tac ctxt) |> K |> Goal.prove ctxt (map fst vars) [] g))
+          phyps
+      val cconj = Thm.cterm_of ctxt pconj
+      val asm = Thm.assume cconj
+      val cfrees = map (Thm.cterm_of ctxt o Free) vars
+      val n = length vars
+      val subs =
+        1 upto n |>
+        map (nth_conv ctxt n) |>
+        map (Drule.instantiate'_normalize (map (SOME o Thm.ctyp_of ctxt o snd) vars) (map SOME cfrees) #> meta_eq)
+      val gthm =
+        fold_rev Thm.implies_intr chyps thm |>
+        fold (curry op RS asm oo curry op RS) intros |>
+        Thm.implies_intr cconj
+      val gprop =
+        gthm |>
+        Thm.prop_of |>
+        Term.subst_free (map (swap o Logic.dest_equals o Thm.prop_of) subs) |>
+        Thm.cterm_of ctxt
+      val gthm =
+        gprop |>
+        Goal.init |>
+        HEADGOAL (rewrite_goal_tac ctxt subs) |>
+        Seq.hd |>
+        pair gthm |>
+        op COMP |>
+        Goal.conclude |>
+        Conv.fconv_rule (Object_Logic.atomize ctxt) |>
+        fold Thm.forall_intr cfrees |>
+        funpow (n - 1) (op CCOMP o rpair @{thm prod.induct})
+    in \<^print> (intros,  gthm)
+    end
+end
+\<close>
+
+ML \<open>
+let
+  val AB = \<^cprop>\<open>A = B\<close>
+  val BC = \<^cprop>\<open>B = C\<close>
+  val thmAB = Thm.assume AB
+  val thmBC = Thm.assume BC
+  val thm = @{thm trans} OF [thmAB, thmBC]
+  val vars = ["A", "B", "C"] |> map (rpair \<^typ>\<open>'a\<close>)
+in
+  Elim_Obtain.export \<^context> vars [AB, BC] thm
+end
+\<close>
+
+consts A :: "'a \<Rightarrow> 'a \<Rightarrow> bool"
+
+lemma xx:"A (fst (snd (snd (x, y, z, w)))) (fst (snd (snd (x, y, z, w))))" sorry
+lemma yy: "(\<And> a b. P (a, b)) \<Longrightarrow> P p" sorry
+ML \<open>@{thm xx} RSN (1, @{thm yy})\<close>
+
+lemma x: "A (fst (snd (snd (x, y, z, w)))) (fst (snd (snd (x, y, z, w))))" sorry
+ML \<open>@{thm x} CCOMP @{thm prod.induct} CCOMP @{thm prod.induct}\<close>
+
+ML \<open>Elim.nth_conv \<^context> 5 5\<close>
+
+lemma "z = fst (snd (snd (x, y, z, w)))"  apply (tactic \<open>HEADGOAL (Elim.prod_sel_tac \<^context>)\<close>)
 
 thm subset_imageE
-thm someI
+thm someI2
 
 ML \<open>Assumption.add_assms\<close>
 
